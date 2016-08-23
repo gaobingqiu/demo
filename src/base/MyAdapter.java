06f0dff703e9e44f5584cf9063f0e260b3a1ccd5
gbq <990924291@qq.com>
2016-08-23 20:01:53 +0800

    image_true

--- src/base/MyAdapter.java
+++ src/base/MyAdapter.java
@@ -2,42 +2,45 @@ package base;
 
 import java.util.List;
 
-import org.apache.http.Header;
-
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.Volley;
 import com.entity.News;
 import com.example.demo.R;
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.http.BitmapCache;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MyAdapter extends BaseAdapter {
 	String tag = "MyAdapter->";
 	private List<News> list;
 	private Context context;
+	private String imgUrl;
 	Drawable drw1;
 	String url;
 	ViewHolder viewHolder;
+	private RequestQueue queue;
+	private ImageLoader imageLoader;
 
 	public MyAdapter(List<News> news, Context context) {
 		// TODO Auto-generated constructor stub
 		this.list = news;
 		this.context = context;
+		queue = Volley.newRequestQueue(context);
+		imageLoader = new ImageLoader(queue, new BitmapCache());
 	}
 
 	public void setData(List<News> news) {
 		this.list = news;
 	}
-	
+
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
@@ -64,7 +67,7 @@ public class MyAdapter extends BaseAdapter {
 		if (convertView == null) {
 			viewHolder = new ViewHolder();
 			convertView = View.inflate(context, R.layout.news_list, null);
-			viewHolder.picUrl = (ImageView) convertView.findViewById(R.id.news_image);
+			viewHolder.img = (NetworkImageView) convertView.findViewById(R.id.news_image);
 			viewHolder.ctime = (TextView) convertView.findViewById(R.id.news_time);
 			viewHolder.url = (TextView) convertView.findViewById(R.id.news_url);
 			viewHolder.url.setVisibility(View.GONE); // 隐藏不参与布局（不占地方）
@@ -73,31 +76,20 @@ public class MyAdapter extends BaseAdapter {
 			// 通过setTag方式 使viewHolder和convertView绑定在一起
 			convertView.setTag(viewHolder);
 		}
+		else {
+			viewHolder = (ViewHolder) convertView.getTag();
+		}
 		viewHolder = (ViewHolder) convertView.getTag();
 		News news = list.get(position);
 		if (null == news.getPicUrl() || news.getPicUrl().isEmpty()) {
 			Log.d(tag, "无图");
 		} else {
-			//加载网络图片begin
-			url = news.getPicUrl();
-			// 创建客户端对象
-			AsyncHttpClient client = new AsyncHttpClient();
-			client.get(url, new AsyncHttpResponseHandler() {
-				@Override
-				public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-					if (statusCode == 200) {
-						BitmapFactory factory = new BitmapFactory();
-						Bitmap bitmap = factory.decodeByteArray(responseBody, 0, responseBody.length);
-						viewHolder.picUrl.setImageBitmap(bitmap);
-					}
-				}
-
-				@Override
-				public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-					error.printStackTrace();
-				}
-			});
-			//加载网络图片end
+			imgUrl = news.getPicUrl();
+			if (imgUrl != null && !imgUrl.equals("")) {
+				viewHolder.img.setDefaultImageResId(R.drawable.ic_launcher);
+				viewHolder.img.setErrorImageResId(R.drawable.ic_launcher);
+				viewHolder.img.setImageUrl(imgUrl, imageLoader);
+			}
 		}
 		viewHolder.ctime.setText(news.getCtime().substring(5));
 		viewHolder.description.setText(news.getDescription());
@@ -108,7 +100,7 @@ public class MyAdapter extends BaseAdapter {
 
 	// 避免重复的findViewById
 	class ViewHolder {
-		public ImageView picUrl;
+		NetworkImageView img;
 		private TextView ctime;
 		private TextView url;
 		private TextView description;
